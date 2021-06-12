e716d3cdf57db081634c0ad5e3f8b7933edc28bb
cosmo fm <56284825+cosmofm@users.noreply.github.com>
2021-06-12 12:51:50 -0700

    Update send-discord-message.py
    got rid of useless line

--- send-discord-message.py
+++ send-discord-message.py
@@ -13,4 +13,3 @@ while True:
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
     }
     requests.post(request_url, data=payload, headers=header)
-    time.sleep(0.5)
